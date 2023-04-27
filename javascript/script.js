dd6f6d0b1009f7c3e2736e057b54831667d874c0
whosenalendra <itsnalendraa@gmail.com>
2023-04-28 05:06:06 +0700

    friday update

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,19 @@
-const navLinks = document.querySelectorAll(".nav a");
+const hamburger = document.querySelector(".hamburger-menu");
+const nav = document.querySelector(".nav");
 
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("change");
+  nav.classList.toggle("active");
+});
+
+const navLinks = document.querySelectorAll(".nav a");
 navLinks.forEach((link) => {
   link.addEventListener("click", (event) => {
     event.preventDefault();
     const targetId = link.getAttribute("href");
     const targetSection = document.querySelector(targetId);
     targetSection.scrollIntoView({ behavior: "smooth" });
+    nav.classList.remove("active");
+    hamburger.classList.remove("change");
   });
-});
-
-const hamburger = document.querySelector(".hamburger-menu");
-const nav = document.querySelector(".nav");
-
-hamburger.addEventListener("click", () => {
-  nav.classList.toggle("show");
 });
